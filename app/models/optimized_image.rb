9beacde856f0fae622825c4bdd273ca17a0ae379
Wellington Cristi Vilela Santana <wellington.c.v.santana@gmail.com>
2026-03-30 13:49:04 -0300

    Revert "FEATURE: automatically downsize large images"

--- app/models/optimized_image.rb
+++ app/models/optimized_image.rb
@@ -139,22 +139,23 @@ class OptimizedImage < ActiveRecord::Base
   end
 
   def self.resize(from, to, width, height, opts={})
-    optimize("resize", from, to, "#{width}x#{height}", opts)
+    optimize("resize", from, to, width, height, opts)
   end
 
   def self.downsize(from, to, max_width, max_height, opts={})
-    optimize("downsize", from, to, "#{max_width}x#{max_height}", opts)
+    optimize("downsize", from, to, max_width, max_height, opts)
   end
 
-  def self.downsize(from, to, dimensions, opts={})
-    optimize("downsize", from, to, dimensions, opts)
-  end
-
-  def self.optimize(operation, from, to, dimensions, opts={})
+  def self.optimize(operation, from, to, width, height, opts={})
+    dim = dimensions(width, height)
     method_name = "#{operation}_instructions"
     method_name += "_animated" if !!opts[:allow_animation] && from =~ /\.GIF$/i
-    instructions = self.send(method_name.to_sym, from, to, dimensions, opts)
+    instructions = self.send(method_name.to_sym, from, to, dim, opts)
     convert_with(instructions, to)
+  end
+
+  def self.dimensions(width, height)
+    "#{width}x#{height}"
   end
 
   def self.convert_with(instructions, to)
